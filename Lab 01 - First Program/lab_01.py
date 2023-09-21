1bcdfc279718d29134f02ec2c90cf4448f3b7276
Jeremy Scruggs <jeremydscruggs1@gmail.com>
2023-09-21 11:43:25 -0400

    lab_02

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
 print("Well, looks like I am running a little late turning this in.")
 print("Maybe I should learn better time management")
-print("But I do my best work under pressure: \"I always have.")
+print("But I do my best work under pressure: \'I always have.'")
 print("However I will try to do better in the future.")
