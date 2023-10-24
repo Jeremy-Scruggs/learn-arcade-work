5500529e242f7a4cf158c92645502313c27c74d1
Jeremy Scruggs <jeremydscruggs1@gmail.com>
2023-10-24 18:19:23 -0400

    Finally got it working

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -32,29 +32,41 @@ def main():
         "You find yourself in the grand hall. An old fireplace crackles. There are doorways to the north and east.", 3,
         1, None, None)
     room_list.append(room)
-
-    room = Room("You are in a long stone hallway. There are doorways to the north, east, and west.", 4, 2, None, 0)
-    room_list.append(room)
-
-    room = Room("You enter the kitchen. The smell of rotten meat fills the air. There are doorways to the "
-                "north and west.", 5, None, None, 1)
-    room_list.append(room)
-
-    room = Room("The pantry is messy and smells funny. There are doorways to the west and south.", None, None, 2, 4)
-    room_list.append(room)
+    # room 1
 
     room = Room(
-        "You step into the bathroom. There is dried blood here. There are doorways to the east and south", None, 4, 0, None)
+        "You are in a long stone hallway. There are doorways to the north, east, and west.", 4, 2, None, 0)
     room_list.append(room)
+    # room 2
+
+    room = Room(
+        "You enter the kitchen. The smell of rotten meat fills the air. There are doorways to the "
+        "north and west.", 5, None, None, 1)
+    room_list.append(room)
+    # room 3
+
+    room = Room(
+        "You step into the bathroom. There is dried blood here. There are doorways to the east and south",
+        None, 4, 0, None)
+    room_list.append(room)
+    # room 4
 
     room = Room(
         "Welcome to the bedroom. A regal bed dominates the room. This room is surprisingly clean compared to "
         "the rest of the temple. There are doorways in all directions.", 6, 5, 1, 3)
     room_list.append(room)
+    # room 5
 
-    room = Room("You enter a crowded armory. Racks line the walls filled to the brim with medieval weaponry. There is a"
-                "doorway to the south.", None, None, 4, None)
+    room = Room(
+        "The pantry is messy and smells funny. There are doorways to the west and south.", None, None, 2, 4)
     room_list.append(room)
+    # room 6
+
+    room = Room(
+        "You enter a crowded armory. Racks line the walls filled to the brim with medieval weaponry. There is a"
+        "doorway to the south.", None, None, 4, None)
+    room_list.append(room)
+    # room 7
 
     current_room = 0
 
